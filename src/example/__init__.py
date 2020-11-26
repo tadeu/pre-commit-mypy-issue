deb18ed76c60b5b27d0a96485c6633a31a93bd72
Tadeu Manoel <e.tadeu@gmail.com>
2020-11-26 15:33:37 -0300

    Add code that fails with `pre-commit mypy` but passes with `mypy`
    (commited with --no-verify)

---
+++ src/example/__init__.py
@@ -0,0 +1,8 @@
+class Example:
+    """
+    This class would have been imported from an external package that desn't have stubs,
+    so the stubs are in `__init__.pyi`.
+    """
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
---
+++ src/example/something.py
@@ -0,0 +1,5 @@
+from example import Example
+
+
+def something() -> Example:
+    return Example(1, 'two')
